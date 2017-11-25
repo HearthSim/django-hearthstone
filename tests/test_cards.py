0996b81c3e17b00dd46938a9142b9c38764b83dc
Jerome Leclanche <jerome@leclan.ch>
2017-11-25 21:06:17 +0200

    Add basic tests and travis configuration

---
+++ tests/test_cards.py
@@ -0,0 +1,9 @@
+import pytest
+from django.core.management import call_command
+from django_hearthstone.cards.models import Card
+
+
+@pytest.mark.django_db
+def test_load_cards():
+	call_command("load_cards")
+	assert Card.objects.count() > 3500
