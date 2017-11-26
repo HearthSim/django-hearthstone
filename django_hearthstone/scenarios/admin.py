b693519a3fcde73ce3a435ba3ed9caa79bf7705d
Jerome Leclanche <jerome@leclan.ch>
2017-11-26 17:02:59 +0200

    Add scenarios module

---
+++ django_hearthstone/scenarios/admin.py
@@ -0,0 +1,52 @@
+from django.contrib import admin
+
+from .models import Adventure, Scenario, Wing
+
+
+class ScenarioInline(admin.TabularInline):
+	model = Scenario
+	fields = ("name", "note_desc", "adventure", "wing", "mode")
+	readonly_fields = fields
+	extra = 0
+	show_change_link = True
+
+
+class WingInline(admin.TabularInline):
+	model = Wing
+	fields = (
+		"name", "note_desc", "ownership_prereq_wing", "coming_soon_label"
+	)
+	readonly_fields = fields
+	extra = 0
+	show_change_link = True
+
+
+@admin.register(Adventure)
+class AdventureAdmin(admin.ModelAdmin):
+	list_display = ("__str__", "note_desc")
+	list_filter = ("leaving_soon", )
+	search_fields = ("name", "note_desc")
+	inlines = (WingInline, ScenarioInline, )
+
+
+@admin.register(Wing)
+class WingAdmin(admin.ModelAdmin):
+	list_display = (
+		"__str__", "adventure", "release", "required_event", "note_desc",
+		"ownership_prereq_wing", "coming_soon_label", "requires_label"
+	)
+	list_filter = ("adventure", )
+	search_fields = ("name", "note_desc")
+	inlines = (ScenarioInline, )
+
+
+@admin.register(Scenario)
+class ScenarioAdmin(admin.ModelAdmin):
+	list_display = (
+		"__str__", "note_desc", "is_tutorial", "is_expert", "is_coop",
+		"players", "adventure", "wing", "mode", "opponent_name",
+	)
+	list_filter = (
+		"adventure", "players", "is_tutorial", "is_expert", "is_coop", "mode"
+	)
+	search_fields = ("name", "note_desc", "description", "opponent_name")
---
+++ django_hearthstone/scenarios/management/commands/load_dbf.py
@@ -0,0 +1,86 @@
+import os
+from argparse import ArgumentTypeError
+from collections import OrderedDict
+
+from django.core.management.base import BaseCommand
+from hearthstone.dbf import Dbf
+
+from ...models import Adventure, Scenario, Wing
+
+
+def build_range(value):
+	value = int(value)
+	if value < 0 or value > 2**32:
+		raise ArgumentTypeError("Invalid build number: %r" % (value))
+	return value
+
+
+class Command(BaseCommand):
+	tables = OrderedDict([
+		("ADVENTURE", Adventure),
+		("WING", Wing),
+		("SCENARIO", Scenario),
+	])
+
+	def add_arguments(self, parser):
+		parser.add_argument("path", nargs=1)
+		parser.add_argument("--build", type=build_range, required=True)
+		parser.add_argument("--force", action="store_true")
+		parser.add_argument("--locale", default="enUS")
+
+	def get_values(self, record, columns):
+		values = {"build": self.build}
+		for column in columns:
+			if isinstance(column, tuple):
+				column, field = column
+			else:
+				field = column.lower()
+			value = record[column]
+			if field.endswith("_id") and value == 0:
+				value = None
+			if value is not None:
+				if isinstance(value, dict):
+					value = value[self.locale]
+				values[field] = value
+
+		return values
+
+	def load_dbf(self, filename):
+		self.stdout.write("Parsing %s" % (filename))
+		dbf = Dbf.load(filename)
+		if dbf.name not in self.tables:
+			self.stderr.write("No handler for %r (%r)" % (filename, dbf.name))
+			return
+
+		cls = self.tables[dbf.name]
+		for record in dbf.records:
+			values = self.get_values(record, cls.dbf_columns)
+			try:
+				instance = cls.objects.get(id=record["ID"])
+			except cls.DoesNotExist:
+				instance = cls.objects.create(**values)
+				self.stdout.write("Created %r (build %r)" % (instance, self.build))
+			else:
+				if self.force or instance.build < self.build:
+					self.stdout.write("Updating %r to build %r" % (instance, self.build))
+					for k, v in values.items():
+						setattr(instance, k, v)
+					instance.save()
+				else:
+					self.stdout.write("Skipping %r (up to date)" % (instance))
+
+	def load_dbf_folder(self, path):
+		for dbf_name in self.tables:
+			filename = os.path.join(path, dbf_name + ".xml")
+			self.load_dbf(filename)
+
+	def handle(self, *args, **options):
+		path = options["path"][0]
+		self.build = options["build"]
+		self.force = options["force"]
+		self.locale = options["locale"]
+
+		if os.path.isdir(path):
+			self.load_dbf_folder(path)
+		else:
+			self.load_dbf(path)
