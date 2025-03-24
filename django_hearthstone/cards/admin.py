28b5230050af0653293f9f703500af92169e1f75
Benedict Etzel <benedict@hearthsim.net>
2025-03-24 15:43:08 +0100

    feat: make CardAdmin readonly

--- django_hearthstone/cards/admin.py
+++ django_hearthstone/cards/admin.py
@@ -19,3 +19,12 @@ class CardAdmin(admin.ModelAdmin):
 	search_fields = (
 		"name", "description", "card_id",
 	)
+
+	def has_add_permission(self, request):
+		return False
+
+	def has_change_permission(self, request, obj=None):
+		return False
+
+	def has_delete_permission(self, request, obj=None):
+		return False
